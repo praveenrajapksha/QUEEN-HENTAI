a42579b4299a1d0b4a23a0b6d5b887c78ad9f2ee
PRAVEEN <141568180+praveenrajapksha@users.noreply.github.com>
2023-08-17 19:10:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94740804536', 'Dinuwa Official²⁰²³', true],
-  ['94740804536', 'Dinuwa Official²⁰²³', true], 
+  ['94753894510', 'praveen', true],
+  ['94753894510', 'praveen', true], 
   [''] 
 ] //Number of owners
 
